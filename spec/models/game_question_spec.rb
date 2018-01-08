1824f03d7970c33a622db7e4825e6f4f55183436
lordsynergy <djordj88@mail.ru>
2018-01-08 18:10:41 +0300

    test for method "correct_answer_key"

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -29,6 +29,10 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.answer_correct?('b')).to be_truthy
     end
 
+    it 'correct .correct_answer_key' do
+      expect(game_question.correct_answer_key).to eq 'b'
+    end
+
 
     #------ Вариант решения ДЗ --------------------
 
